bb74b089e9086b6f026263977efa8d9ac82f3b9b
hgkm5768 <hgkm5768@gmail.com>
2023-11-04 16:31:53 +0000

    implement simple alarm

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -44,10 +44,7 @@ struct thread* get_thread_from_telem(struct list_elem* elem) {
 bool tick_less (const struct list_elem *a,
                 const struct list_elem *b,
                 void *aux UNUSED) {
-  if(get_ticks_from_elem(a) != get_ticks_from_elem(b))
-    return get_ticks_from_elem(a) < get_ticks_from_elem(b);
-  else
-    return get_thread_from_telem(a)->priority > get_thread_from_telem(b)->priority; // prj3: give priority to alarm
+  return get_ticks_from_elem(a) < get_ticks_from_elem(b);
 }
 
 struct list timer_list;
